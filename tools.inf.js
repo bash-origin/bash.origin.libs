a0ff35333d83cc265f65592d9a0181d104711795
Christoph Dorn <christoph@christophdorn.com>
2018-10-15 21:09:06 -0700

    wip

--- tools.inf.js
+++ tools.inf.js
@@ -10,6 +10,56 @@ exports.inf = async function (INF, ALIAS) {
     const repository = SIMPLE_GIT(__dirname);
     INF.LIB.Promise.promisifyAll(repository);
 
+    const packsPath = INF.LIB.PATH.join(__dirname, 'packs');
+    const packNames = await INF.LIB.FS.readdirAsync(packsPath);
+
+
+
+    function makeVersionIncrementer () {
+
+        const segmentChanged = {
+            "major": false,
+            "minor": false,
+            "patch": false,
+            "prerelease": false
+        };
+
+        return {
+            check: function (beforeVersion, afterVersion) {
+
+                if (SEMVER.major(beforeVersion) !== SEMVER.major(afterVersion)) {
+                    segmentChanged.major = true;
+                } else
+                if (SEMVER.minor(beforeVersion) !== SEMVER.minor(afterVersion)) {
+                    segmentChanged.minor = true;
+                } else
+                if (SEMVER.patch(beforeVersion) !== SEMVER.patch(afterVersion)) {
+                    segmentChanged.patch = true;
+                } else
+                if (JSON.stringify(SEMVER.prerelease(beforeVersion)) !== JSON.stringify(SEMVER.prerelease(afterVersion))) {
+                    segmentChanged.prerelease = true;
+                }
+            },
+            incrementVersion: function (version) {
+
+                if (segmentChanged.major) {
+                    version = SEMVER.inc(version, "major");
+                } else
+                if (segmentChanged.minor) {
+                    version = SEMVER.inc(version, "minor");
+                } else
+                if (
+                    segmentChanged.patch ||
+                    segmentChanged.prerelease
+                ) {
+                    version = SEMVER.inc(version, "patch");
+                }
+                return version;
+            }
+        };
+    }
+
+
 
     async function updatePackage (basePath) {
 
@@ -24,12 +74,7 @@ exports.inf = async function (INF, ALIAS) {
 
         const descriptorAfter = await INF.LIB.FS.readJSONAsync(descriptorPath);
 
-        const segmentChanged = {
-            "major": false,
-            "minor": false,
-            "patch": false,
-            "prerelease": false
-        };
+        const versionIncrementer = makeVersionIncrementer();
 
         [
             'dependencies',
@@ -47,36 +92,12 @@ exports.inf = async function (INF, ALIAS) {
                 const afterVersion = SEMVER.coerce(descriptorAfter[type][name]).version;
 
                 console.log(`[bash.origin.lib] Package version selector '${name}' has changed from '${descriptorBefore[type][name]}' to '${descriptorAfter[type][name]}'`);
-                
-                if (SEMVER.major(beforeVersion) !== SEMVER.major(afterVersion)) {
-                    segmentChanged.major = true;
-                } else
-                if (SEMVER.minor(beforeVersion) !== SEMVER.minor(afterVersion)) {
-                    segmentChanged.minor = true;
-                } else
-                if (SEMVER.patch(beforeVersion) !== SEMVER.patch(afterVersion)) {
-                    segmentChanged.patch = true;
-                } else
-                if (SEMVER.prerelease(beforeVersion) !== SEMVER.prerelease(afterVersion)) {
-                    segmentChanged.prerelease = true;
-                }
+
+                versionIncrementer.check(beforeVersion, afterVersion);
             });
         });
 
-        let version = descriptorAfter.version;
-
-        if (segmentChanged.major) {
-            version = SEMVER.inc(version, "major");
-        } else
-        if (segmentChanged.minor) {
-            version = SEMVER.inc(version, "minor");
-        } else
-        if (
-            segmentChanged.patch ||
-            segmentChanged.prerelease
-        ) {
-            version = SEMVER.inc(version, "patch");
-        }
+        let version = versionIncrementer.incrementVersion(descriptorAfter.version);
 
         if (version === descriptorAfter.version) {
             return;
@@ -113,8 +134,8 @@ exports.inf = async function (INF, ALIAS) {
 
                 if (value.value === "update") {
 
-                    if ((await repository.statusAsync()).staged.length) {
-                        throw new Error("Cannot update as there are staged git changes! Remove staged changes first.");
+                    if ((await repository.statusAsync()).modified.length) {
+                        throw new Error("Cannot update as there are modified files in git! Commit modifications first.");
                     }
 
 
@@ -126,23 +147,53 @@ exports.inf = async function (INF, ALIAS) {
 
                     console.log("[bash.origin.lib] Updating packs ...");
 
-                    const packsPath = INF.LIB.PATH.join(__dirname, 'packs');
-                    const packs = await INF.LIB.FS.readdirAsync(packsPath);
+                    await INF.LIB.Promise.mapSeries(packNames, async function (packName) {
 
-                    await INF.LIB.Promise.mapSeries(packs, async function (pack) {
+                        console.log(`[bash.origin.lib] Updating packages for pack '${packName}' ...`);
 
-                        console.log(`[bash.origin.lib] Updating packages for pack '${pack}' ...`);
-
-                        const version = await updatePackage(INF.LIB.PATH.join(packsPath, pack));
-                        await commitIfChanged(`packs/${pack}/package.json`, `Bumped '${pack}' pack version to '${version}'`);
+                        const version = await updatePackage(INF.LIB.PATH.join(packsPath, packName));
+                        await commitIfChanged(`packs/${packName}/package.json`, `Bumped '${packName}' pack version to '${version}'`);
                     });
 
 
                 } else
                 if (value.value === "release-preview") {
 
+                    if ((await repository.statusAsync()).modified.length) {
+                        throw new Error("Cannot release preview as there are modified files in git! Commit modifications first.");
+                    }
 
+                    const versionIncrementer = makeVersionIncrementer();
 
+                    const latestTag = (await repository.tagsAsync()).latest;
+
+                    await INF.LIB.Promise.mapSeries(packNames, async function (packName) {
+                        versionIncrementer.check(
+                            JSON.parse(await repository.showAsync([`${latestTag}:packs/${packName}/package.json`])).version,
+                            JSON.parse(await repository.showAsync([`master:packs/${packName}/package.json`])).version
+                        );
+                    });
+            
+                    const descriptorPath = INF.LIB.PATH.join(__dirname, 'package.json');
+                    const descriptor = await INF.LIB.FS.readJSONAsync(descriptorPath);
+
+                    let version = versionIncrementer.incrementVersion(descriptor.version);
+
+                    if (!/-pre\.\d+$/.test(version)) {
+                        version += "-pre.0";
+                    } else {
+                        version = SEMVER.inc(version, "prerelease");
+                    }
+
+                    descriptor.version = version;
+
+                    await INF.LIB.FS.outputFileAsync(descriptorPath, JSON.stringify(descriptor, null, 2) + "\n", "utf8");
+
+                    await commitIfChanged(`package.json`, `Bumped 'bash.origin.lib' package version to '${version}'`);
+
+// TODO: Commit changes, tag, create npm package and push.
+
+                    return true;
                 }
 
                 return true;
